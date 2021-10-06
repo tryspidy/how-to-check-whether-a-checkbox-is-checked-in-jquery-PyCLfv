4ca0ddec8363f2bccd3b09155018fdf71f816806
tryspidy <68438592+tryspidy@users.noreply.github.com>
2021-10-07 01:30:05 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+//using plane javascript 
+if(document.getElementById('on_or_off_checkbox').checked) {
+    //I am checked
+} 
+
+//using jQuery
+if($('#on_or_off_checkbox').is(':checked')){
+    //I am checked
+}
